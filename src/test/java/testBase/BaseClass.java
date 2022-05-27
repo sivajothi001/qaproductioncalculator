2bf27df85e453eae92a8f7e3b44533c26a146c0e
sivajothieswaran <sivajothieswaran@chenmacl55>
2022-05-27 17:30:29 +0530

    123

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -58,7 +58,7 @@ public class BaseClass {
 			 
 			log.info("Launching Chrome Browser");
 						
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			
 			//System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") +"/drivers/chromedriver.exe");  
 			
